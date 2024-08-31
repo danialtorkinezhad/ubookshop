836e8832dbaa69bbda9d35009836cd55b6fb06de
danialtorkinezhad <$danaltorkinezhad@gmail.com>
2024-08-31 20:42:18 +0330

    Update

--- components/Pages/Block.tsx
+++ components/Pages/Block.tsx
@@ -1,94 +1,87 @@
 export default (props) => {
-    if (!props.state.faves) {
-      props.state.faves = [];
-    }
-    let book = props.book;
-  
-    return (
-      <g-cse
-        onClick={() => {
-          props.state.form = "details";
-          props.state.book = book;
-          props.refresh();
-        }}
+  if (!props.state.faves) {
+    props.state.faves = [];
+  }
+  let book = props.book;
+
+  return (
+    <g-cse
+      onClick={() => {
+        props.state.form = "details";
+        props.state.book = book;
+        props.refresh();
+      }}
+      style={{
+        height: "400",
+        border: "solid lightgray",
+        marginTop: "15px",
+        borderRadius: "15px",
+        backgroundColor: "white",
+        boxShadow: "20px 20px 25px ",
+      }}
+    >
+      <img
+        className={global.styles.hoverzoom_nofade}
+        src={book.imageLink}
         style={{
-          height:"400",
-          border:"solid lightgray",
-          marginTop:"15px",
-          borderRadius: "15px",
-          backgroundColor: "white",
-          boxShadow: "20px 20px 25px " , 
-          
+          borderRadius: "15px 15px 0px 0px",
+          height: 250,
+          width: 200,
+          objectFit: "cover",
+        }}
+      />
+
+      <f-cc
+        style={{
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          paddingTop: 9,
+          paddingBottom: 0,
         }}
       >
-        <img
-          className={global.styles.hoverzoom_nofade}
-          src={book.imageLink}
-          style={{
-            borderRadius: "15px 15px 0px 0px",
-            height: 250,
-            width: 200,
-            objectFit: "cover",
-          }}
-        />
-  
-        <f-cc
-          style={{
-            width: "100%",
-            display: "flex",
-            flexDirection: "column",
-            paddingTop: 9,
-            paddingBottom:0,
-           
-          }}
-         >
-          <f-13 style={{ fontFamily: "vrb" , fontWeight:"bold" }}>{book.title}</f-13>
-          <f-cc style={{ width: "100%" }}>
-            {props.state.faves.includes(props.book.title) ? (
-              
-              <img
-                src="https://irmapserver.ir/research/33/check.png"
-                alt=""
-                style={{
-                  objectFit: "contain",
-                  width: 70,
-                  marginRight: 5,
-                  
-                }}
-              />
-            ) : (
-              <img
-                src="https://irmapserver.ir/qepal/cart.svg"
-                style={{
-                  objectFit: "contain",
-                  width: 65,
-                  marginRight: 5,
-                  paddingBottom:"0px"
-                }}
-              />
-            )}
-            <c-cc
+        <f-13 style={{ fontFamily: "vrb", fontWeight: "bold" }}>
+          {book.title}
+        </f-13>
+        <f-cc style={{ width: "100%" }}>
+          {props.state.faves.includes(props.book.title) ? (
+            <img
+              src="https://irmapserver.ir/research/33/check.png"
+              alt=""
               style={{
-                display: "flex",
-                marginTop:"30px",
-                padding: 20,
-                width: "100%",
+                objectFit: "contain",
+                width: 70,
+                marginRight: 5,
               }}
-             >
-             
-                <del style={{ fontSize: 14 , color:"black", fontFamily:"vrb"}}>
-                  
-                  {(book.price as number).toLocaleString("fa-IR")} تومان
-                </del>
-                <p style={{color:"green" , fontFamily:"vrb", fontSize:17}}>
-                  
-                  {((book.price * 0.75) as number).toLocaleString("fa-IR")} تومان
-                    
-              </p>
-              
-            </c-cc>
-          </f-cc>
+            />
+          ) : (
+            <img
+              src="https://irmapserver.ir/qepal/cart.svg"
+              style={{
+                objectFit: "contain",
+                width: 65,
+                marginRight: 5,
+                paddingBottom: "0px",
+              }}
+            />
+          )}
+          <c-cc
+            style={{
+              display: "flex",
+              marginTop: "30px",
+              padding: 20,
+              width: "100%",
+            }}
+          >
+            <del style={{ fontSize: 14, color: "black", fontFamily: "vrb" }}>
+              {(book.price as number).toLocaleString("fa-IR")} تومان
+            </del>
+            <p style={{ color: "green", fontFamily: "vrb", fontSize: 17 }}>
+              {((book.price * 0.75) as number).toLocaleString("fa-IR")} تومان
+            </p>
+          </c-cc>
         </f-cc>
-      </g-cse>
-    );
-  };
+      </f-cc>
+    </g-cse>
+  );
+};
--- components/Pages/index.tsx
+++ components/Pages/index.tsx
@@ -14,44 +14,80 @@ const Page: PageEl = (props, state, refresh, getProps) => {
   let styles = global.styles;
   let sum = 0;
   let count = 0;
-  state.faves = Array.from(new Set(state.faves))
+  state.faves = Array.from(new Set(state.faves));
   for (let faves of state.faves) {
-    let book = props.books.find(ketab => ketab.title == faves)
-    if (book) { sum = sum + (book.price) * 0.9 }
+    let book = props.books.find((ketab) => ketab.title == faves);
+    if (book) {
+      sum = sum + book.price * 0.75;
+    }
     count = count + 1;
   }
 
-
-
-
   return (
-    <div style={{ direction: "rtl", minHeight: "11vh", backgroundColor: "GrayText" }}>
+    <div
+      style={{
+        direction: "rtl",
+        minHeight: "11vh",
+        backgroundColor: "GrayText",
+      }}
+    >
       <br-x />
 
-
       <c-cc>
-        <Window title=' سبد خرید ' style={{ height: 150, width: "97%", fontFamily: "vrb", backgroundColor: " lightgray" }}>
+        <Window
+          title=" سبد خرید "
+          style={{
+            height: 150,
+            width: "97%",
+            fontFamily: "vrb",
+            backgroundColor: " lightgray",
+          }}
+        >
           <f-cse style={{ paddingTop: 35 }}>
-
-
-            <f-cc style={{ backgroundColor: "white", height: "60px", width: "30%", borderRadius: "8px", boxShadow: "15px 15px 20px" }}> <img src="https://irmapserver.ir/research/77/money.png"
-              style={{ width: "3rem" }}></img> &nbsp; <f-20 style={{ fontFamily: "vrb" }}> مجموع پرداخت : {sum.toLocaleString("fa-IR")} </f-20>
+            <f-cc
+              style={{
+                backgroundColor: "white",
+                height: "60px",
+                width: "30%",
+                borderRadius: "8px",
+                boxShadow: "15px 15px 20px",
+              }}
+            >
+              {" "}
+              <img
+                src="https://irmapserver.ir/research/77/money.png"
+                style={{ width: "3rem" }}
+              ></img>{" "}
+              &nbsp;{" "}
+              <f-20 style={{ fontFamily: "vrb" }}>
+                {" "}
+                مجموع پرداخت : {sum.toLocaleString("fa-IR")}{" "}
+              </f-20>
             </f-cc>
 
-            <f-cc style={{ backgroundColor: "white", height: "60px", width: "30%", borderRadius: "8px", boxShadow: "15px 15px 20px" }}> <img src="https://irmapserver.ir/research/77/book.png"
-              style={{ width: "3rem" }}></img> &nbsp; <f-20 style={{ fontFamily: "vrb" }}> تعداد کتاب : {count.toLocaleString("fa-IR")} </f-20>
+            <f-cc
+              style={{
+                backgroundColor: "white",
+                height: "60px",
+                width: "30%",
+                borderRadius: "8px",
+                boxShadow: "15px 15px 20px",
+              }}
+            >
+              {" "}
+              <img
+                src="https://irmapserver.ir/research/77/book.png"
+                style={{ width: "3rem" }}
+              ></img>{" "}
+              &nbsp;{" "}
+              <f-20 style={{ fontFamily: "vrb" }}>
+                {" "}
+                تعداد کتاب : {count.toLocaleString("fa-IR")}{" "}
+              </f-20>
             </f-cc>
-
-
           </f-cse>
         </Window>
       </c-cc>
-
-
-
-
-
-
 
       {state.form == "details" ? (
         <WindowFloat
@@ -63,7 +99,7 @@ const Page: PageEl = (props, state, refresh, getProps) => {
           }}
         >
           <f-c>
-            <f-15 >نام کتاب : </f-15>
+            <f-15>نام کتاب : </f-15>
             <sp-2 />
             <f-15>{state.book.title}</f-15>
           </f-c>
@@ -99,25 +135,34 @@ const Page: PageEl = (props, state, refresh, getProps) => {
                 state.faves = [];
               }
 
-
               if (state.faves.includes(state.book.title)) {
-                state.faves = state.faves.filter(item => item !== state.book.title)
-                state.form = null
+                state.faves = state.faves.filter(
+                  (item) => item !== state.book.title
+                );
+                state.form = null;
               } else {
-                state.faves.push(state.book.title)
-                state.form = null
+                state.faves.push(state.book.title);
+                state.form = null;
               }
 
               refresh();
             }}
           >
-            <f-15 style={{ color: "lightgray", fontFamily: "vrb" }}> خرید </f-15>
+            <f-15 style={{ color: "lightgray", fontFamily: "vrb" }}>
+              {" "}
+              خرید{" "}
+            </f-15>
           </g-b>
         </WindowFloat>
-      ) : null}<Window
+      ) : null}
+      <Window
         title={" کتاب ها  "}
         style={{
-          minHeight: 200, margin: 10, width: "calc(100% - 20px)", fontFamily: "vrb", backgroundColor: " lightgray",
+          minHeight: 200,
+          margin: 10,
+          width: "calc(100% - 20px)",
+          fontFamily: "vrb",
+          backgroundColor: " lightgray",
           // backgroundImage: 'url(${ "https://cdn.prod.website-files.com/5f64854a08f25af4fc8763b6/622b2a8c5ae8dd46d414abf1_bookshop.jpg"})'
         }}
       >
@@ -128,15 +173,33 @@ const Page: PageEl = (props, state, refresh, getProps) => {
         </w-cse>
       </Window>
     </div>
-  )
-}
+  );
+};
 
 export async function getServerSideProps(context) {
-  var session = await global.SSRVerify(context)
-  var { uid, name, image, imageprop, lang, cchar,
-    unit, workspace, servid, servsecret,
-    usedquota, quota, quotaunit, status, regdate, expid,
-    role, path, devmod, userip, } = session;
+  var session = await global.SSRVerify(context);
+  var {
+    uid,
+    name,
+    image,
+    imageprop,
+    lang,
+    cchar,
+    unit,
+    workspace,
+    servid,
+    servsecret,
+    usedquota,
+    quota,
+    quotaunit,
+    status,
+    regdate,
+    expid,
+    role,
+    path,
+    devmod,
+    userip,
+  } = session;
 
   let books = await global.db.collection("books").find({}).toArray();
 
